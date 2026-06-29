12ca63f7b091f2fd8e27bee03384a20294a2bb02
Lahiru Dissanayake <lahirudissanayake15@gmail.com>
2026-06-29 10:37:42 +0530

    feat: add start-all script to streamline frontend and backend development setup

---
+++ start-all.ps1
@@ -0,0 +1,4 @@
+Start-Process cmd.exe -ArgumentList "/k cd taskforge-frontend && npm run dev"
+Start-Process cmd.exe -ArgumentList "/k cd taskforge-backend && npm run dev"
+
+# Start-Process powershell -ArgumentList "cd taskforge-frontend; npm run dev"
